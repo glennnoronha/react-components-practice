7bc22d9b551f66c88f6747741ea3e32acd8b64a1
glennnoronha <glenn.noronha@yahoo.com>
2025-09-21 15:00:28 -0500

    finished to do list component

--- components/ToDo.jsx
+++ components/ToDo.jsx
@@ -1,39 +1,90 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function ToDo() {
-  const [counter, setCounter] = useState(0);
+  const [tasks, setTasks] = useState(["Get Groceries", "Pay Bills", "Go Gym"]);
+  const [newTask, setNewTask] = useState("");
 
-  const decrease = () => {
-    setCounter(counter - 1);
-    console.log("decrease working");
-  };
+  function handleInput(event) {
+    setNewTask(event.target.value);
+  }
 
-  const increase = () => {
-    setCounter(counter + 1);
-  };
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
 
-  const reset = () => {
-    setCounter(0);
-  };
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function moveUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]]
+      setTasks(updatedTasks)
+    }
+  }
+
+  function moveDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]]
+      setTasks(updatedTasks)
+    }
+  }
+
   return (
-    <div className=" flex flex-col gap-6">
+    <div className=" flex flex-col gap-6 w-2xl">
       <div>
         <h3 className="text-4xl font-bold text-center">To-Do List</h3>
-        <form htmlFor="new-todo-input" className="flex flex-col gap-4">
-          <label>
-            <p className="text-xl font-bold">What needs to be done?</p>
-          </label>
+        <div className="flex gap-2 mt-6">
           <input
             type="text"
-            id="new-todo-input"
             name="text"
-            className="bg-gray-800"
+            className="bg-gray-700 rounded p-2 flex-grow"
+            placeholder="Enter a task..."
+            value={newTask}
+            onChange={handleInput}
           />
-        </form>
+          <button className="bg-black rounded p-2 " onClick={addTask}>
+            Add Task
+          </button>
+        </div>
+        <ol>
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              className="flex items-center gap-2 py-2 my-2 border-1 rounded"
+            >
+              <span className="flex-grow rounded p-2">{task}</span>
+              <button
+                className="bg-red-500 p-2 rounded"
+                onClick={() => deleteTask(index)}
+              >
+                Delete
+              </button>
+              <button
+                className="text-4xl rounded"
+                onClick={() => moveUp(index)}
+              >
+                ⬆️
+              </button>
+              <button
+                className="text-4xl rounded"
+                onClick={() => moveDown(index)}
+              >
+                ⬇️
+              </button>
+            </li>
+          ))}
+        </ol>
       </div>
     </div>
   );
